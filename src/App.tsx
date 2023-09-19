dcdf7dddbb3318bbbbf40dec187cceecfe920ef2
yoohaaeun <101792909+yoohaaeun@users.noreply.github.com>
2023-09-19 16:09:35 +0900

    Side Logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
+import SideLogo from './components/SideLogo';
 
 function App() {
   return (
     <>
       <Navbar />
+      <SideLogo />
       <Outlet />
       <Footer />
     </>
---
+++ src/components/SideLogo.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function SideLogo() {
+  return (
+    <img
+      src='/images/ep.png'
+      alt=''
+      className='w-12 fixed left-2 top-1/2 transform -translate-y-1/2 z-50 -rotate-90'
+    />
+  );
+}
