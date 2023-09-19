539c2fe087b073e6eeff39b021648e87944f5a8d
yoohaaeun <101792909+yoohaaeun@users.noreply.github.com>
2023-09-19 16:10:11 +0900

    Home 스타일링

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <>
+      <img
+        src='/images/main.jpg'
+        alt=''
+        className='w-screen h-screen object-cover object-center'
+      />
+      <div className='h-60 bg-main' />
+    </>
+  );
+}
