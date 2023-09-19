5a4cc8c630e680c74d1fc6e1520a6c3d0692b2f0
yoohaaeun <101792909+yoohaaeun@users.noreply.github.com>
2023-09-19 21:57:27 +0900

    로그인, 로그아웃 기능 구현

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { motion, AnimatePresence } from 'framer-motion';
+import { login, logout, onUserStateChange } from '../api/firebase';
 
 const dropdownVariants = {
   hidden: { y: -20, opacity: 0 },
@@ -15,23 +16,46 @@ const dropdownVariants = {
 };
 
 export default function Navbar() {
-  const [isOvered, setHovered] = useState(false);
   const location = useLocation();
+  const [user, setUser] = useState<any>();
+  const [isShopHovered, setIsShopHovered] = useState(false);
+  const [isAccountHovered, setIsAccountHovered] = useState(false);
 
-  const handleMouseEnter = () => {
-    setHovered(true);
+  useEffect(() => {
+    onUserStateChange((user: any) => {
+      console.log(user);
+      setUser(user);
+    });
+  }, []);
+
+  const handleLogin = () => {
+    login().then(setUser);
+  };
+
+  const handleLogout = () => {
+    logout().then(setUser);
+  };
+
+  const handleMouseEnter = (menuType: string) => {
+    if (menuType === 'shop') {
+      setIsShopHovered(true);
+      setIsAccountHovered(false);
+    } else if (menuType === 'account') {
+      setIsAccountHovered(true);
+      setIsShopHovered(false);
+    }
   };
 
   const handleMouseLeave = () => {
-    setHovered(false);
+    setIsShopHovered(false);
+    setIsAccountHovered(false);
   };
 
   return (
     <header className='fixed top-0 left-0 w-full bg-transparent z-10'>
       <nav className='flex justify-between items-start p-5 font-medium'>
-        <Link
-          to='/products'
-          onMouseEnter={handleMouseEnter}
+        <div
+          onMouseEnter={() => handleMouseEnter('shop')}
           onMouseLeave={handleMouseLeave}
           className={`relative  ${
             location.pathname === '/' ? 'text-white' : 'text-black'
@@ -39,30 +63,30 @@ export default function Navbar() {
         >
           Shop
           <AnimatePresence>
-            {isOvered && (
+            {isShopHovered && (
               <motion.ul
                 variants={dropdownVariants}
                 initial='hidden'
                 animate='visible'
                 exit='hidden'
-                className='absolute py-5  text-xs leading-6 whitespace-nowrap'
+                className='absolute py-5  text-xs leading-6 whitespace-nowrap cursor-pointer'
               >
-                <Link to='/products/new'>
-                  <motion.li>New</motion.li>
-                </Link>
+                <motion.li>
+                  <Link to='/products/new'>New </Link>
+                </motion.li>
                 <motion.li>Outerwear</motion.li>
                 <motion.li>Knitwear</motion.li>
-                <motion.li>Top</motion.li>
+                <motion.li>Tops</motion.li>
                 <motion.li>Dresses</motion.li>
                 <motion.li>Accessories</motion.li>
               </motion.ul>
             )}
           </AnimatePresence>
-        </Link>
+        </div>
         <p
           className={`${
             location.pathname === '/' ? 'text-white' : 'text-black'
-          }`}
+          } hover:border-b transition-all duration-300 ease-in-out`}
         >
           Collection
         </p>
@@ -73,18 +97,37 @@ export default function Navbar() {
             className='w-full relative bottom-3'
           />
         </Link>
-        <button
+        <div
+          onMouseEnter={() => handleMouseEnter('account')}
+          onMouseLeave={handleMouseLeave}
           className={`${
             location.pathname === '/' ? 'text-white' : 'text-black'
-          }`}
+          } hover:border-b transition-all duration-300 ease-in-out`}
         >
-          Login
-        </button>
+          Account
+          <AnimatePresence>
+            {isAccountHovered && (
+              <motion.ul
+                variants={dropdownVariants}
+                initial='hidden'
+                animate='visible'
+                exit='hidden'
+                className='absolute py-5  text-xs leading-6 whitespace-nowrap cursor-pointer'
+              >
+                {!user && <motion.li onClick={handleLogin}>Login</motion.li>}
+                {user && <motion.li onClick={handleLogout}>Logout</motion.li>}
+                <motion.li>My Page</motion.li>
+                <motion.li>Wish List</motion.li>
+                <motion.li>제품 등록</motion.li>
+              </motion.ul>
+            )}
+          </AnimatePresence>
+        </div>
         <Link
           to='/carts'
           className={`${
             location.pathname === '/' ? 'text-white' : 'text-black'
-          }`}
+          } hover:border-b transition-all duration-300 ease-in-out`}
         >
           Bag
         </Link>
