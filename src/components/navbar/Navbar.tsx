f09da0ee161dba06aec6d0a11bb64d78afcb2424
yoohaaeun <101792909+yoohaaeun@users.noreply.github.com>
2023-10-10 12:25:46 +0900

    navbar 스타일링 추가

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,3 +1,4 @@
+import { useLocation } from 'react-router-dom';
 import Shop from './Shop';
 import Collection from './Collection';
 import Logo from './Logo';
@@ -5,8 +6,14 @@ import Account from './Account';
 import Bag from './Bag';
 
 export default function Navbar() {
+  const location = useLocation();
+
   return (
-    <header className='fixed top-0 left-0 w-full bg-transparent z-10'>
+    <header
+      className={`fixed top-0 left-0 w-full bg-transparent z-10 ${
+        location.pathname !== '/' && 'bg-gradient-to-b from-white'
+      }`}
+    >
       <nav className='grid grid-cols-navColumns gap-2 whitespace-nowrap px-2 sm:px-5 py-5 text-sm sm:text-base md:text-lg font-medium'>
         <Shop />
         <Collection />
