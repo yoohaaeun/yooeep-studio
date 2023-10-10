a20f71b682499a10b0a7594a54b133b95adaaf7b
yoohaaeun <101792909+yoohaaeun@users.noreply.github.com>
2023-10-10 19:04:38 +0900

    footer 내용 수정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,14 @@
 export default function Footer() {
   return (
     <footer className='w-full h-28 md:h-40 flex justify-center items-center gap-2 border-t'>
-      <img
-        src='images/transparent-yooeep.png'
-        alt='Yooeep Logo'
-        className='w-20'
-      />
-      <p className='text-xs text-gray-500'>© 2023 Yooeep Studio</p>
+      <a href='https://github.com/yoohaaeun/yooeep-studio'>
+        <img
+          src='images/transparent-yooeep.png'
+          alt='Yooeep Logo'
+          className='w-20'
+        />
+      </a>
+      <p className='text-xs text-gray-500'>© 2023 Yooeep Studio.</p>
     </footer>
   );
 }
