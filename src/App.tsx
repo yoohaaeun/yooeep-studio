966c09ee655d1143373f368abb972a4ac66aeb17
yoohaaeun <101792909+yoohaaeun@users.noreply.github.com>
2023-10-11 15:59:20 +0900

    페이지 이동 시 스크롤을 맨 위로 이동하는 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AuthContextProvider } from './context/AuthContext';
 import Footer from './components/Footer';
 import Navbar from './components/navbar/Navbar';
 import SideLogo from './components/SideLogo';
+import { useEffect } from 'react';
 
 const queryClient = new QueryClient();
 
 function App() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <QueryClientProvider client={queryClient}>
       <AuthContextProvider>
