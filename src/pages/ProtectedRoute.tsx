66b26087a4e294e65201832156e4c3c1c6bac397
yoohaaeun <101792909+yoohaaeun@users.noreply.github.com>
2023-11-01 23:14:11 +0900

    유저 미로그인 및 권한 부족시 알림 메시지 추가

--- src/pages/ProtectedRoute.tsx
+++ src/pages/ProtectedRoute.tsx
@@ -20,7 +20,13 @@ export default function ProtectedRoute({
     return <LoadingSpinner />;
   }
 
-  if (!user || (requireAdmin && !user.isAdmin)) {
+  if (!user) {
+    alert('로그인 후 이용해주세요.');
+    return <Navigate to='/' replace />;
+  }
+
+  if (requireAdmin && !user.isAdmin) {
+    alert('관리자 권한이 필요합니다. 권한이 없습니다.');
     return <Navigate to='/' replace />;
   }
 
